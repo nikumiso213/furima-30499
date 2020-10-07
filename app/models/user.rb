ad6dd1bedd3eb9b9fbb225e58c9a6f79afcad69f
y-nakamura <mugi001@icloud.com>
2020-10-07 16:02:11 +0900

    usersモデルにバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-end
+
+  validates :nickname, presence: true
+  with_options presence: true , format: {with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "全角文字を入力してください"} do 
+    validates :family_name, presence: true
+    validates :first_name, presence: true
+  end
+  with_options presence: true, format: {with: /\A[ァ-ン]+\z/, message: "全角（カタカナ）で入力してください"} do
+    validates :family_name_reading, presence: true
+    validates :first_name_reading, presence: true
+  end
+  validates :birthday, presence: true
+ end

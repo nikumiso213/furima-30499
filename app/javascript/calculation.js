e356e79a9cb00ea7c3cacc3c98aa54a9491dd1f4
y-nakamura <mugi001@icloud.com>
2020-10-08 18:01:48 +0900

    販売手数料・利益計算機能の実装

---
+++ app/javascript/calculation.js
@@ -0,0 +1,12 @@
+function price() {
+  const itemPrice = document.getElementById("item-price");
+
+  itemPrice.addEventListener("keyup", () => {
+    const taxPrice = Math.floor(itemPrice.value * 0.1);
+    const profit = itemPrice.value - taxPrice;
+    document.getElementById("add-tax-price").innerHTML = taxPrice;
+    document.getElementById("profit").innerHTML = profit;
+  });
+}
+
+window.addEventListener("load", price)

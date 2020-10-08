847c05627654666ca462cf5ca46523563256889f
y-nakamura <mugi001@icloud.com>
2020-10-08 17:13:42 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,16 @@
 class Item < ApplicationRecord
+  belongs_to :user
+
+  has_one_attached :image
+
+  with_options presence: true  do
+    validates :name
+    validates :description
+    validates :category_id
+    validates :status_id
+    validates :shopping_fee_id
+    validates :prefecture_id
+    validates :delivery_schedule_id
+    validates :price, numericality: {greater_than_or_equal_to: 300, less_than: 10000000}, format: {with: /\A[0-9]+\z/, message: "は半角数字で入力してください"}
+  end
 end

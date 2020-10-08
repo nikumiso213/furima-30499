530a3b8077cec18ed171573c65def2f02666d0a4
y-nakamura <mugi001@icloud.com>
2020-10-08 12:13:43 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
+  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'は半角英字と数字の両方を含めてください'}
   with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を入力してください' } do
-    validates :family_name, presence: true
-    validates :first_name, presence: true
+    validates :family_name
+    validates :first_name
   end
   with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角（カタカナ）で入力してください' } do
-    validates :family_name_reading, presence: true
-    validates :first_name_reading, presence: true
+    validates :family_name_reading
+    validates :first_name_reading
   end
   validates :birthday, presence: true
 end

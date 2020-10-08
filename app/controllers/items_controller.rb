5d34306fdcfc0348353cea4cbc4010ab2d9fad8d
y-nakamura <mugi001@icloud.com>
2020-10-08 18:01:13 +0900

    コントローラーのアクションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,10 +4,20 @@ class ItemsController < ApplicationController
   end
 
   def new
-
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(params_item)
+    if @item.save
+      redirect_to action: :index
+    end
+      render :new
+  end
 
+  private
+
+  def params_item
+    params.require(:item).permit(:name, :description, :category_id, :status_id, :shopping_fee_id, :prefecture_id, :delivery_schedule_id, :price, :image).merge(user_id: current_user.id)
   end
 end

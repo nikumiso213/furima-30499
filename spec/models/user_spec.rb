ec07b9e0ec3e21e19f40659311ce3be7fb278d37
y-nakamura <mugi001@icloud.com>
2020-10-08 14:08:18 +0900

    単体テストコード修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,12 +27,6 @@ RSpec.describe User, type: :model do
     expect(@user.errors.full_messages).to include('Email is invalid')
   end
 
-  it 'emailが空の場合登録できないこと' do
-    @user.email = nil
-    @user.valid?
-    expect(@user.errors.full_messages).to include("Email can't be blank")
-  end
-
   it '重複したemailが存在する場合登録できないこと' do
     @user.save
     another_user = FactoryBot.build(:user)
@@ -59,8 +53,14 @@ RSpec.describe User, type: :model do
     expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
   end
 
-  it 'passwordが半角英数字混合でない場合登録できないこと' do
+  it 'passwordが半角英字のみの場合登録できないこと' do
     @user.password = 'passix'
+    @user.valid?
+    expect(@user.errors.full_messages).to include('Password は半角英字と数字の両方を含めてください')
+  end
+
+  it 'passwordが半角数字のみの場合登録できないこと' do
+    @user.password = "123456"
     @user.valid?
     expect(@user.errors.full_messages).to include('Password は半角英字と数字の両方を含めてください')
   end

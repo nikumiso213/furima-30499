5066bcab1fe0fd2514a84cd74ab73b8cf9a81078
y-nakamura <mugi001@icloud.com>
2020-10-12 16:50:15 +0900

    空のメソッドの削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,12 +20,6 @@ class ItemsController < ApplicationController
     end
   end
 
-  def show
-  end
-
-  def edit
-  end
-
   def update
     if @item.update(params_item)
       redirect_to actin: :index

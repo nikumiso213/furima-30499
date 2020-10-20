7dace3d25bd93d61050e49195c831105bd74e9fe
y-nakamura <mugi001@icloud.com>
2020-10-20 18:44:50 +0900

    Commentテーブルの単体テスト実装

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :comment do
-    
+    content { Faker::Lorem.paragraph }
+    association :item
+    association :user
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    before do
+      @comment = FactoryBot.build(:comment)
+    end
+
+    it "contentが存在すれば保存できること" do
+      expect(@comment).to be_valid
+    end
+
+    it "contentが存在しない場合保存できないこと" do
+      @comment.content = nil
+      @comment.valid?
+      expect(@comment.errors.full_messages).to include("コメントを入力してください")
+    end
+
+    it "userと紐づいていない場合保存できないこと" do
+      @comment.user = nil
+      @comment.valid?
+      expect(@comment.errors.full_messages).to include("Userを入力してください")
+    end 
+
+    it "itemと紐づいていない場合保存できないこと" do
+      @comment.item = nil
+      @comment.valid?
+      expect(@comment.errors.full_messages).to include("Itemを入力してください")
+    end 
 end

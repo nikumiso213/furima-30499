b805848c61a3106d9e7f096d2596c111b3471f17
y-nakamura <mugi001@icloud.com>
2020-10-20 15:31:22 +0900

    バリデーション ・アソシエーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   has_many :items
   has_many :purchases
+  has_many :comments
 
   validates :nickname, presence: true
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'は半角英字と数字の両方を含めてください' }
